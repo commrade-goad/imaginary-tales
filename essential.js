39d5dfe3e3399db371f4ad3ca6b7dbb113a80bee
Fernando Perry <fernandoperry1234@gmail.com>
2023-09-28 08:53:01 +0700

    small change to name

--- essential.js
+++ essential.js
@@ -30,15 +30,13 @@ newDivHeader.innerHTML = `
 `;
 
 newDivFooter.innerHTML = `
-    <footer>
-        <p><i class="nf nf-md-github"></i> <a href="https://github.com/commrade-goad/imaginary-tales">Source Code</a></p>
-    </footer>
+    <p><i class="nf nf-md-github"></i> <a href="https://github.com/commrade-goad/imaginary-tales">Source Code</a></p>
 `;
 
 
 // Get the element in the existing DOM where you want to insert the new content
 var containerElementHeader = document.getElementById('top-bar'); // Replace 'container' with the ID of the container element you want to use
-var containerElementFooter = document.getElementById('footer-bar'); // Replace 'container' with the ID of the container element you want to use
+var containerElementFooter = document.getElementById('bottom-bar'); // Replace 'container' with the ID of the container element you want to use
 
 // Append the new div to the container element
 containerElementHeader.appendChild(newDivHeader);
