345e196b3b9c1727a5e42548c06ae5a2073d802d
Fernando Perry <fernandoperry1234@gmail.com>
2023-12-16 20:48:02 +0700

    update adminpage by @MashuNakamura

--- admin/adminpage.php
+++ admin/adminpage.php
@@ -10,19 +10,61 @@ if (!isset($_SESSION['username'])) {
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Admin Page</title>
-</head>
-<body>
-    <h2>Welcome, <?php echo $_SESSION['username']; ?>!</h2>
-    <p>This is the admin page content.</p>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Admin Page</title>
+    </head>
+    <body>
+        <center>
+            <h2>Welcome, <?php echo $_SESSION['username']; ?>!</h2>
+            <p>This is the admin page content.</p>
+            <?php
+            // Define the PHP function
+            function myFunction() {
+            // Your PHP function logic here
+            return "Function executed successfully!";
+            }
+
+            // Check if the button is clicked
+            if (isset($_POST['submit'])) {
+            // Call the PHP function when the button is clicked
+            $result = myFunction();
+            echo $result;
+            }
+            ?>
+
+            <form method="post">
+                <input type="submit" name="deletebook" value="Delete Book" id="button">
+                <input type="submit" name="addbook" value="Add Book" id="button">
+            </form>
+
+            <style>
+
+html {
+    margin-top: 200px;
+    font-size:15px;
+    font-family:sans-serif;
+}
+
+h2 {
+    border-bottom:5px solid black;
+    width:15%;
+    padding-bottom:10px;
+}
+
+#button {
+    padding:10px 5px;
+    background:linear-gradient(100deg, darkgray, gray);
+}
+
+            </style>
+        </center>
+    </body>
 </html>
 
 <?php
-$_SESSION = array();
-session_destroy();
+// $_SESSION = array();
+// session_destroy();
 // header("location: login.php");
 // exit();
 ?>
